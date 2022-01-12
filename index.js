56a9963042a6a34b48543de07836d410e51f9e18
MarkWanjauMoringa <mark.wanjau@student.moringaschool.com>
2022-01-12 05:17:02 +0300

    let fuctions and math

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ $('#submit').on('click', () => {
     day = date.getDate();
     month = date.getMonth() + 1;
     year = date.getFullYear();
+    let yearString = String(year);
+    let century = parseInt(yearString.slice(0,2));
+    let calculatingYear = parseInt(yearString.slice(2,5));
+    let day_of_week = (((century/4)-2*century-1) + ((5*calculatingYear/4)) + ((26*(month+1)/10)) + day) % 7;
+    let weekday = Math.trunc(day_of_week);
+    
