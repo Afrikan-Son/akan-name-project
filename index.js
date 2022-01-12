149b2333ba3e9e58890676ec03462bb5775fecc5
MarkWanjauMoringa <mark.wanjau@student.moringaschool.com>
2022-01-12 04:21:56 +0300

    let

---
+++ index.js
@@ -0,0 +1,8 @@
+let day;
+let month;
+let year;
+let akanName;
+
+let boys = new Array("Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame");
+
+let girls = new Array("Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama");
