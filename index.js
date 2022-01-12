47fd79cacc42bc7b90d155f0c27f3599faf9affb
MarkWanjauMoringa <mark.wanjau@student.moringaschool.com>
2022-01-12 04:57:58 +0300

    submit click

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ let akanName;
 let boys = new Array("Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame");
 
 let girls = new Array("Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama");
+
+$('#submit').on('click', () => {
+    var date = new Date($('#birthdate').val());
+    var gender = $("input[name='gender']:checked").val();
